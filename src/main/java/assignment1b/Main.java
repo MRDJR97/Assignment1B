9b988291aa41e1b6658726a2cf667eb3a890a7e2
DESKTOP-SC3VO8I\Daniel <danieljohnregan@gmail.com>
2018-10-29 21:50:00 +0000

    Another commit

---
+++ src/main/java/assignment1b/Main.java
@@ -0,0 +1,13 @@
+package assignment1b;
+
+import assignment1.Student;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("STARTING");
+		Student s = new Student();
+	}
+
+}
